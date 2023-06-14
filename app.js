8e28184f463eaba4f3a5398f2d801d2c03d98be3
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2023-06-14 16:10:11 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,26 @@
+/*//jshint esversion:6
+
+const express = require("express");
+const app = express();*/
+const links = document.querySelectorAll('.link');
+const sections = document.querySelectorAll('section');
+
+let activeLink = 0;
+
+links.forEach((link, i) => {
+    link.addEventListener('click', () => {
+        if(activeLink != i){
+            links[activeLink].classList.remove('active');
+            link.classList.add('active');
+            sections[activeLink].classList.remove('active');
+
+            setTimeout(() => {
+                activeLink = i;
+                sections[i].classList.add('active');
+            }, 1000);
+        }
+    })
+})
+/*app.listen(7000,function(){
+  console.log("Server is running on port 7000.")
+})*/
